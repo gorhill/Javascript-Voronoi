24d33a0cae7c7bb1daa60ec68fc4430a0ec59836
Raymond Hill <rhill@raymondhill.net>
2011-05-20 17:12:12 -0400

    new version: now using red-black tree instead of plain array for Beachline, improved usage to minimize data duplication

--- rhill-voronoi-demo1.php
+++ rhill-voronoi-demo1.php
@@ -13,7 +13,7 @@ body > div > div {margin:0;border:1px solid #ccc;border-top:0;padding:4px;}
 h1 {font:bold 20px sans-serif;margin:0 0 0.5em 0;padding:4px;background-color:#c9d7f1;}
 h4 {font-size:14px;margin:0.5em 0 0 0;border:0;border-bottom:solid 1px #c9d7f1;padding:2px;background-color:#e5ecf9;}
 #canvasParent {margin-top:0;margin-bottom:1em;padding:0;border:0}
-#voronoiCode {font:11px monospace;overflow:auto;color:gray;}
+#voronoiCode {font:11px monospace;overflow:auto;color:#666;}
 #voronoiCode span {color:green;font-weight:bold;}
 </style>
 <script type="text/javascript">
@@ -36,6 +36,7 @@ _gaq.push(['_trackPageview']);
 var VoronoiDemo = {
 
 	voronoi: new Voronoi(),
+	sites: [],
 	diagram: null,
 	margin: 50,
 	canvas: null,
@@ -48,29 +49,22 @@ var VoronoiDemo = {
 		},
 
 	clearSites: function() {
-		this.voronoi.setSites([]);
-		this.diagram = this.voronoi.compute(this.bbox);
+		this.sites = [];
+		this.diagram = this.voronoi.compute(this.sites, this.bbox);
 		this.updateStats();
 		},
 
 	randomSites: function(n,clear) {
+		if (clear) {this.sites = [];}
 		// create vertices
 		var xo = this.margin;
 		var dx = this.canvas.width-this.margin*2;
 		var yo = this.margin;
 		var dy = this.canvas.height-this.margin*2;
-		var vertices = [];
 		for (var i=0; i<n; i++) {
-			vertices.push({x:self.Math.round((xo+self.Math.random()*dx)*10)/10,y:self.Math.round((yo+self.Math.random()*dy)*10)/10});
+			this.sites.push({x:self.Math.round((xo+self.Math.random()*dx)*10)/10,y:self.Math.round((yo+self.Math.random()*dy)*10)/10});
 			}
-		// make sites from vertices and compute resulting Voronoi diagram
-		if (clear) {
-			this.voronoi.setSites(vertices);
-			}
-		else {
-			this.voronoi.addSites(vertices);
-			}
-		this.diagram = this.voronoi.compute(this.bbox);
+		this.diagram = this.voronoi.compute(this.sites, this.bbox);
 		this.updateStats();
 		},
 
@@ -78,7 +72,7 @@ var VoronoiDemo = {
 		if (!this.diagram) {return;}
 		var e = document.getElementById('voronoiStats');
 		if (!e) {return;}
-		e.innerHTML = '('+this.diagram.cells.numCells+' Voronoi cells computed from '+this.diagram.sites.length+' Voronoi sites in '+this.diagram.execTime+' ms &ndash; rendering <i>not</i> included)';
+		e.innerHTML = '('+this.diagram.cells.numCells+' Voronoi cells computed from '+this.diagram.cells.numCells+' Voronoi sites in '+this.diagram.execTime+' ms &ndash; rendering <i>not</i> included)';
 		},
 
 	render: function() {
@@ -107,7 +101,7 @@ var VoronoiDemo = {
 			ctx.lineTo(v.x,v.y);
 			}
 		// sites
-		var sites = this.diagram.sites;
+		var sites = this.sites;
 		var nSites = sites.length;
 		for (var iSite=nSites-1; iSite>=0; iSite-=1) {
 			v = sites[iSite];
@@ -122,7 +116,7 @@ var VoronoiDemo = {
 <body onload="VoronoiDemo.init();">
 <h1>Javascript implementation of Steven Fortune's algorithm to compute Voronoi diagrams<br/>Demo 1: measuring peformance</h1>
 <div id="divroot" style="width:800px;">
-<p style="margin-top:0;"><a href="/voronoi/rhill-voronoi.php">&lt; Back to main page</a> | <b>Demo 1: measuring peformance</b> | <a href="/voronoi/rhill-voronoi-demo2.php">Demo 2: a bit of interactivity</a></p>
+<p style="margin-top:0;"><a href="/voronoi/rhill-voronoi.php">&lt; Back to main page</a> | <b>Demo 1: measuring peformance</b> | <a href="rhill-voronoi-demo2.php">Demo 2: a bit of interactivity</a> | <a href="rhill-voronoi-demo3.php">Demo 3: Fancy tiling</a></p>
 <h4 class="divhdr">Sites generator</h4>
 <div class="divinfo" id="voronoiGenerator">
 <input type="button" value="Generate" onclick="VoronoiDemo.randomSites(parseInt(document.getElementById('voronoiNumberSites').value,10),true);VoronoiDemo.render();"/> or <input type="button" value="Add" onclick="VoronoiDemo.randomSites(parseInt(document.getElementById('voronoiNumberSites').value,10),false);VoronoiDemo.render();"/><input id="voronoiNumberSites" type="text" value="100" size="5" maxlength="5"/> sites randomly (Warning: performance might suffer the more sites you add.)
@@ -144,95 +138,92 @@ var VoronoiDemo = {
 
 ...
 
-&lt;script type=&quot;text/javascript&quot;&gt;
+<?php
+echo htmlentities(<<<EOT
+<script type="text/javascript">
+<!--
 var VoronoiDemo = {
 
-  <span>voronoi: new Voronoi(),</span>
-  diagram: null,
-  margin: 50,
-  canvas: null,
-  <span>bbox: {xl:0,xr:800,yt:0,yb:600},</span>
+	voronoi: new Voronoi(),
+	sites: [],
+	diagram: null,
+	margin: 50,
+	canvas: null,
+	bbox: {xl:0,xr:800,yt:0,yb:600},
 
-  init: function() {
-    this.canvas = document.getElementById('voronoiCanvas');
-    this.randomSites(100,true);
-    this.render();
-    },
+	init: function() {
+		this.canvas = document.getElementById('voronoiCanvas');
+		this.randomSites(100,true);
+		this.render();
+		},
 
-  clearSites: function() {
-    <span>this.voronoi.setSites([]);</span>
-    <span>this.diagram = this.voronoi.compute(this.bbox);</span>
-    this.updateStats();
-    },
+	clearSites: function() {
+		this.sites = [];
+		this.diagram = this.voronoi.compute(this.sites, this.bbox);
+		this.updateStats();
+		},
 
-  randomSites: function(n,clear) {
-    // create vertices
-    var xo = this.margin;
-    var dx = this.canvas.width-this.margin*2;
-    var yo = this.margin;
-    var dy = this.canvas.height-this.margin*2;
-    var vertices = [];
-    for (var i=0; i&lt;n; i++) {
-      vertices.push({x:self.Math.round(xo+self.Math.random()*dx),y:self.Math.round(yo+self.Math.random()*dy)});
-      }
-    // make sites from vertices and compute resulting Voronoi diagram
-    if (clear) {
-      <span>this.voronoi.setSites(vertices);</span>
-      }
-    else {
-      <span>this.voronoi.addSites(vertices);</span>
-      }
-    <span>this.diagram = this.voronoi.compute(this.bbox);</span>
-    this.updateStats();
-    },
+	randomSites: function(n,clear) {
+		if (clear) {this.sites = [];}
+		// create vertices
+		var xo = this.margin;
+		var dx = this.canvas.width-this.margin*2;
+		var yo = this.margin;
+		var dy = this.canvas.height-this.margin*2;
+		for (var i=0; i<n; i++) {
+			this.sites.push({x:self.Math.round((xo+self.Math.random()*dx)*10)/10,y:self.Math.round((yo+self.Math.random()*dy)*10)/10});
+			}
+		this.diagram = this.voronoi.compute(this.sites, this.bbox);
+		this.updateStats();
+		},
 
-  updateStats: function() {
-    if (!this.diagram) {return;}
-    var e = document.getElementById('voronoiStats');
-    if (!e) {return;}
-    e.innerHTML = '('+this.diagram.cells.numCells+' Voronoi cells computed from '+this.diagram.sites.length+' Voronoi sites in '+this.diagram.execTime+' ms &ndash; rendering &lt;i&gt;not&lt;/i&gt; included)';
-    },
+	updateStats: function() {
+		if (!this.diagram) {return;}
+		var e = document.getElementById('voronoiStats');
+		if (!e) {return;}
+		e.innerHTML = '('+this.diagram.cells.numCells+' Voronoi cells computed from '+this.diagram.cells.numCells+' Voronoi sites in '+this.diagram.execTime+' ms &ndash; rendering <i>not</i> included)';
+		},
 
-  render: function() {
-    var ctx = this.canvas.getContext('2d');
-    // background
-    ctx.globalAlpha = 1;
-    ctx.beginPath();
-    ctx.rect(0,0,this.canvas.width,this.canvas.height);
-    ctx.fillStyle = '#fff';
-    ctx.fill();
-    ctx.strokeStyle = '#888';
-    ctx.stroke();
-    // voronoi
-    <span>if (!this.diagram) {return;}</span>
-    ctx.strokeStyle='#000';
-    ctx.beginPath();
-    // edges
-    <span>var edges = this.diagram.edges;</span>
-    var nEdges = edges.length;
-    var edge, v;
-    for (var iEdge=nEdges-1; iEdge&gt;=0; iEdge--) {
-      edge = edges[iEdge];
-      v = edge.va;
-      ctx.moveTo(v.x,v.y);
-      v = edge.vb;
-      ctx.lineTo(v.x,v.y);
-      }
-    // sites
-    <span>var sites = this.diagram.sites;</span>
-    var nSites = sites.length;
-    for (var iSite=nSites-1; iSite&gt;=0; iSite--) {
-      v = sites[iSite];
-      ctx.rect(v.x-0.5,v.y-0.5,1,1);
-      }
-    ctx.stroke();
-    },
-  };
-&lt;/script&gt;
-
-...
-
-&lt;body onload=&quot;VoronoiDemo.init();&quot;&gt;
+	render: function() {
+		var ctx = this.canvas.getContext('2d');
+		// background
+		ctx.globalAlpha = 1;
+		ctx.beginPath();
+		ctx.rect(0,0,this.canvas.width,this.canvas.height);
+		ctx.fillStyle = '#fff';
+		ctx.fill();
+		ctx.strokeStyle = '#888';
+		ctx.stroke();
+		// voronoi
+		if (!this.diagram) {return;}
+		ctx.strokeStyle='#000';
+		ctx.beginPath();
+		// edges
+		var edges = this.diagram.edges;
+		var nEdges = edges.length;
+		var edge, v;
+		for (var iEdge=nEdges-1; iEdge>=0; iEdge-=1) {
+			edge = edges[iEdge];
+			v = edge.va;
+			ctx.moveTo(v.x,v.y);
+			v = edge.vb;
+			ctx.lineTo(v.x,v.y);
+			}
+		// sites
+		var sites = this.sites;
+		var nSites = sites.length;
+		for (var iSite=nSites-1; iSite>=0; iSite-=1) {
+			v = sites[iSite];
+			ctx.rect(v.x-0.5,v.y-0.5,1,1);
+			}
+		ctx.stroke();
+		},
+	};
+// -->
+</script>
+EOT
+, ENT_QUOTES);
+?>
 
 ...
 </pre>
