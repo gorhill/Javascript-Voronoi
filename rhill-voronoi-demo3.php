a4a5785f184ae98d4ab8d42bea24c67008770019
Raymond Hill <rhill@raymondhill.net>
2011-05-26 13:46:14 -0400

    lol @ 'algorotaterithm', no idea what happened

--- rhill-voronoi-demo3.php
+++ rhill-voronoi-demo3.php
@@ -579,7 +579,7 @@ window.addEvent('domready',function(){VoronoiDemo.init();});
 </head>
 <body>
 <a href="http://github.com/gorhill/Javascript-Voronoi"><img style="position:absolute;top:0;right:0;border:0;" src="https://d3nwyuy0nl342s.cloudfront.net/img/7afbc8b248c68eb468279e8c17986ad46549fb71/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub"></a>
-<h1>Javascript implementation of Steven Fortune's algorotaterithm to compute Voronoi diagrams<br/>Demo 3: Fancy tiling</h1>
+<h1>Javascript implementation of Steven Fortune's algorithm to compute Voronoi diagrams<br/>Demo 3: Fancy tiling</h1>
 <div id="divroot">
 <p style="margin-top:0;"><a href="/voronoi/rhill-voronoi.php">&lt; Back to main page</a> | <a href="rhill-voronoi-demo1.php">Demo 1: measuring peformance</a> | <a href="rhill-voronoi-demo2.php">Demo 2: a bit of interactivity</a> | <b>Demo 3: Fancy tiling</b> | <a href="http://www.raymondhill.net/blog/?p=458#comments">Comments</a></p>
 <div class="pane" id="canvasParent">
