2c3e5ce5f5e9c443ed2f49b05624998d5fdc27c0
Raymond Hill <rhill@raymondhill.net>
2011-05-26 16:36:43 -0400

    Fixed erroneous explanation in comment

--- rhill-voronoi-core.js
+++ rhill-voronoi-core.js
@@ -630,11 +630,11 @@ Voronoi.prototype.Halfedge = function(edge, lSite, rSite) {
 	this.edge = edge;
 	// 'angle' is a value to be used for properly sorting the
 	// halfsegments counterclockwise. By convention, we will
-	// use the angle of the perpendicular line to the line defined
-	// by the 'site to the left' to the 'site to the right'.
-	// However, border edges have no 'site to the right': use angle
-	// of perpendicular to halfsegment (the edge should have both
-	// end points defined in such case.)
+	// use the angle of the line defined by the 'site to the left'
+	// to the 'site to the right'.
+	// However, border edges have no 'site to the right': thus we
+	// use the angle of line perpendicular to the halfsegment (the
+	// edge should have both end points defined in such case.)
 	if (rSite) {
 		this.angle = Math.atan2(rSite.y-lSite.y, rSite.x-lSite.x);
 		}
