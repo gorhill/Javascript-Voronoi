26776525060183cd47fae20d9885538f114dbbfe
ajuvercr <arthur.vercruysse@ugent.be>
2020-04-18 14:08:54 +0200

    Update rhill-voronoi-core.d.ts
    Also export the inner classes

--- rhill-voronoi-core.d.ts
+++ rhill-voronoi-core.d.ts
@@ -1,53 +1,56 @@
-declare class Point {
-    x: number;
-    y: number;
-}
 
-declare class Site {
-    x: number;
-    y: number;
-    voronoiId: number;
-}
+declare namespace Voronoi {
+    class Point {
+        x: number;
+        y: number;
+    }
 
-declare class Cell {
-    site: Site;
-    halfedges: HalfEdge[];
-    closeMe: boolean;
-}
+    class Site {
+        x: number;
+        y: number;
+        voronoiId: number;
+    }
 
-declare class Edge {
-    lSite: Site;
-    rSite: Site;
-    vb: Point;
-    va: Point;
-}
+    class Cell {
+        site: Site;
+        halfedges: HalfEdge[];
+        closeMe: boolean;
+    }
 
-declare class HalfEdge {
-    site: Site;
-    edge: Edge;
-    angle: number;
-    getStartpoint(): Point;
-    getEndpoint(): Point;
-}
+    class Edge {
+        lSite: Site;
+        rSite: Site;
+        vb: Point;
+        va: Point;
+    }
 
-declare class BBox {
-    xl: number;
-    xr: number;
-    yt: number;
-    yb: number;
-}
+    class HalfEdge {
+        site: Site;
+        edge: Edge;
+        angle: number;
+        getStartpoint(): Point;
+        getEndpoint(): Point;
+    }
 
-declare class VoronoiDiagram {
-    site: any;
-    cells: Cell[];
-    edges: Edge[];
-    vertices: Point[];
-    execTime: number;
+    class BBox {
+        xl: number;
+        xr: number;
+        yt: number;
+        yb: number;
+    }
+
+    class VoronoiDiagram {
+        site: any;
+        cells: Cell[];
+        edges: Edge[];
+        vertices: Point[];
+        execTime: number;
+    }
 }
 
 declare class Voronoi {
     constructor();
-    compute(sites: Point[], bbox: BBox): VoronoiDiagram;
+    compute(sites: Voronoi.Point[], bbox: Voronoi.BBox): Voronoi.VoronoiDiagram;
 }
 
 export = Voronoi;
